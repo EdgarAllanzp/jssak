fd25869aab4d91e2db1a2f88032b2785abceee45
EdgarAllanzp <edgarallan@qq.com>
2018-12-28 22:56:36 +0800

    Add capitalize.js.

--- docs/generator.js
+++ docs/generator.js
@@ -1,22 +1,31 @@
 const fs = require('fs');
 const Comments = require('parse-comments');
 
-const comments = new Comments();
+function extractComment(path) {
+  const comment = {};
+  const commentsInfo = new Comments();
+  const ast = commentsInfo.parse(fs.readFileSync(path, 'utf-8'))[0];
+  const [syntax, ...params] = ast.tags;
+  const returns = params.pop();
 
-comments.on('rawNode', function(node) {
-  console.log('rawNode');
-  console.log(node);
-});
+  comment.params = params.map(param => {
+    return {
+      name: param.name,
+      type: param.type.name,
+      description: param.description
+    };
+  });
+  comment.syntax = syntax.description;
+  comment.description = ast.description;
+  comment.returns = {
+    type: returns.type.name,
+    description: returns.description
+  };
+  comment.examples = ast.examples[0].raw;
 
-comments.on('token', function(tok) {
-  console.log('token');
-  console.log(tok);
-  if (tok.example.code && !tok.example.lang) {
-    tok.example.lang = 'js';
-  }
-});
+  return comment;
+}
 
-const ast = comments.parse(fs.readFileSync('./src/array/is-sorted.js', 'utf8'));
-// console.log(ast);
-// console.log(ast[0].tags[1]);
-console.log(ast[0].examples[0]);
+// let comment = extractComment('./src/array/is-sorted.js');
+
+// console.log(comment);
